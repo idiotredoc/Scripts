e0cfb60f9c0d709788ba01cbe57551a751af8237
Nathan Lutz <nathan.lutz@resiteonline.com>
2013-03-11 14:00:11 -0400

    updated etckeeper install script

--- installers/install_etckeeper.sh
+++ installers/install_etckeeper.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+ARCH=`uname -m`
+RELEASE=`cat /etc/redhat-release | awk '{ print substr($3,1,1)}'`
+TBI=epel-release-5-4.noarch.rpm
+
 echo "Installing Etc-keeper and prereqs"
+echo "Checking for EPEL"
+repolist | grep epel
+if [ $? == 1]; then
+echo "Installing EPEL"
+sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/$RELEASE/$ARCH/$TBI
+fi
+echo "Installing etckeeper, bzr"
 sudo yum install etckeeper bzr -y
 echo "Initializing git repo"
 cd /etc
