4317474ded210fe0530b15e9dcbd431f5778bf35
Nathan Lutz <nathan.lutz@resiteonline.com>
2013-03-11 14:11:46 -0400

    updated etckeeper install script

--- installers/install_etckeeper.sh
+++ installers/install_etckeeper.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+set -e
 ARCH=`uname -m`
 RELEASE=`cat /etc/redhat-release | awk '{ print substr($3,1,1)}'`
 TBI=epel-release-5-4.noarch.rpm
 
 echo "Installing Etc-keeper and prereqs"
 echo "Checking for EPEL"
-repolist | grep epel
-if [ $? == 1]; then
+yum repolist | grep epel
+if [ $? == "1" ]; then
 echo "Installing EPEL"
-sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/$RELEASE/$ARCH/$TBI
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/$RELEASE/$ARCH/$TBI
 fi
 echo "Installing etckeeper, bzr"
-sudo yum install etckeeper bzr -y
+yum install etckeeper bzr -y
 echo "Initializing git repo"
 cd /etc
-sudo etckeeper init
+etckeeper init
 echo "First commit!"
-sudo etckeeper vcs commit -m "First commit"
+etckeeper vcs commit -m "First commit"
 echo "Now check the status..." 
-sudo etckeeper vcs status
+etckeeper vcs status
