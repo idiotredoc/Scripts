4057b208f1688296c384436260874f59f1c1160c
Nathan Lutz <nathan.lutz@resiteonline.com>
2013-03-15 16:26:02 -0400

    update backup of apache

--- backups/backup_resite_apache.sh
+++ backups/backup_resite_apache.sh
@@ -6,9 +6,18 @@ if [ -z "${WHICHTYPE}" ];
 then
         WHICHTYPE="daily"
 fi
-if [ "$WHICHTYPE}" -eq "daily" ];
+if [ "$WHICHTYPE" = "weekly" ];
 then
-	/bin/tar -cpzf $DIR/clientSites-$date.tar.gz /var/www --exclude "/var/www/desi" --exclude "/var/www/images" --exclude "/var/www/cache" --exclude "/var/www/toBeDeleted" --exclude "/var/www/coreBeta" --exclude "/var/www/coreStaging" --exclude "/var/www/coreTest" --exclude "/var/www/archive"
-        scp $DIR/clientSites-$date.tar.gz backup@dsrscbkp01:~/resite/archive/.
-	rm -rf $DIR/*
+	tar -cpzf $DIR/clientSites-full-$date.tar.gz /var/www --exclude "/var/www/images" --exclude "/var/www/cache" --exclude "/var/www/toBeDeleted" --exclude "/var/www/coreBeta" --exclude "/var/www/coreStaging" --exclude "/var/www/coreTest" --exclude "/var/www/archive"
+        scp $DIR/clientSites-$date.tar.gz backup@dsrscbkp01:~/resite/archive/apache/.
+	ssh backup@dsrscbkp01 "find /archive/primary/resite/apache/clientSites-full* -mtime +5 -exec rm {} \;"
+        rm -rf $DIR/*
+elif [ "$WHICHTYPE" = "daily" ];
+then
+        find /var/www \( -name cache -o -name images -o -name core -o -name toBeDeleted -o -name archive -o -name coreStaging -o -name coreTest -o -name coreBeta -\) -prune -o -mtime -2 -print0  |xargs -0 tar -pcf $DIR/clientSites-incr-$DATE.tar.gz
+        scp $DIR/clientSites-incr-$DATE.tar.bz2 backup@dsrscbkp01:~/resite/archive/apache/.
+        ssh backup@dsrscbkp01 "find /archive/primary/resite/apache/clientSites-incr-* -mtime +7 -exec rm {} \;"
+        rm -rf $DIR/*
 fi
+
+
