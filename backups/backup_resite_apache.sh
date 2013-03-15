de085e45ee2ae1abe15a6f644efdd99c206b6f58
Nathan Lutz <nathan.lutz@resiteonline.com>
2013-03-15 15:08:52 -0400

    added apache backup, files backup and cleanup shell script

---
+++ backups/backup_resite_apache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+DATE=`/bin/date '+%Y%m%d'`
+DIR=/tmp/apache
+WHICHTYPE="$1"
+if [ -z "${WHICHTYPE}" ];
+then
+        WHICHTYPE="daily"
+fi
+if [ "$WHICHTYPE}" -eq "daily" ];
+then
+	/bin/tar -cpzf $DIR/clientSites-$date.tar.gz /var/www --exclude "/var/www/desi" --exclude "/var/www/images" --exclude "/var/www/cache" --exclude "/var/www/toBeDeleted" --exclude "/var/www/coreBeta" --exclude "/var/www/coreStaging" --exclude "/var/www/coreTest"
+        scp $DIR/clientSites-$date.tar.gz backup@dsrscbkp01:~/resite/archive/.
+	rm -rf $DIR/*
+fi
---
+++ backups/backup_resite_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+DATE=`/bin/date '+%Y%m%d'`
+DIR=/tmp/files/
+WHICHTYPE="$1"
+if [ -z "${WHICHTYPE}" ];
+then
+        WHICHTYPE="daily"
+fi
+if [ "${WHICHTYPE}" -eq "monthly" ];
+then
+	tar -jcvf $DIR/resite_archive_full_$DATE.tar.bz2 /archive/leasing-applications /archive/recolor /archive/uploads
+	scp $DIR/resite_archive_full_$DATE.tar.bz2 backup@dsrscbkp01:~/resite/archive/.
+	rm -rf $DIR/*
+elif [ "$WHICHTYPE}" -eq "daily" ];
+then
+	find /archive/uploads -type f -mtime -2 -print0 |xargs -0 tar -pcf $DIR/archive_uploads_incr_$DATE.tar.gz
+	find /archive/leasing-applications -type f -mtime -2 -print0 |xargs -0 tar -pcf $DIR/archive_leaseApps_incr_$DATE.tar.gz
+	find /archive/recolor -type f -mtime -2 -print0 |xargs -0 tar -pcf $DIR/archive_recolor_incr_$DATE.tar.gz
+        tar -jcvf $DIR/resite_archive_incr_$DATE.tar.bz2 $DIR/archive_uploads_incr_$DATE.tar.gz $DIR/archive_leaseApps_incr_$DATE.tar.gz $DIR/archive_recolor_incr_$DATE.tar.gz
+        scp $DIR/resite_archive_incr_$DATE.tar.bz2 backup@dsrscbkp01:~/resite/archive/.
+	rm -rf $DIR/*
+fi
+
